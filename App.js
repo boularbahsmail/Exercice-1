2d675c00b8e697bdeb1d79d8094229ff3d97d73a
Ismail Boularbah <qiranorane55@gmail.com>
2021-02-04 15:41:00 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,32 @@
+
+// Copyright : © Ismail Boularbah - 2021
+// Programming language used : JavaScript
+
+// Question :
+
+// Write a script that allow us to remove a specific
+// element from a given array
+
+// We have an array that contains numbers.
+// We need to write a script that allow us
+// to remove a specific value from the given array
+
+// Use any programming language you want assat (sata)
+// Baddal extension dyal l file ela hsab programming language li atkhdem(i) biha :)
+
+// Array
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Main function
+// Arrow function
+const remove = () => {
+    // Your code here
+}
+
+// Calling the function
+remove();
+
+// Mli tdir(i) clone l repo, dir(i) ta share l post f facebook :)
+// Good luck ♥
+// © 2021 Made with ❤️ by Boularbah Ismail
+
